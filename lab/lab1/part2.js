790f5d7d5566f010f0fc64f50cd06c238c54a7f2
jiaxuanlyu <70524710+jiaxuanlyu@users.noreply.github.com>
2021-02-10 18:34:50 +0800

    update lab1 part2
    finish some questions of lab1 part2

--- lab/lab1/part2.js
+++ lab/lab1/part2.js
@@ -7,7 +7,7 @@ Functions that `return` can be passed as values to other functions. Each exercis
 Instructions: Write a function that *always* returns the number 1.
 ===================== */
 
-var justOne = function() {};
+var justOne = function() { return 1};
 
 console.log('justOne success:', justOne() === 1);
 
@@ -15,7 +15,7 @@ console.log('justOne success:', justOne() === 1);
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-var isEven = function() {};
+var isEven = function(num) { return num % 2 === 0 };
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -24,7 +24,7 @@ Instructions: Write a function that *always* returns false.
               Use functions "justOne" and "isEven" somehow in the definition.
 ===================== */
 
-var justFalse = function() {};
+var justFalse = function() {return false};
 
 console.log('justFalse success:', justFalse() === false);
 
@@ -32,7 +32,7 @@ console.log('justFalse success:', justFalse() === false);
 Instructions: Write a function that takes a boolean value and returns its opposite.
 ===================== */
 
-var not = function() {};
+var not = function(val) {return !val};
 
 console.log('not success:', not(true) === false);
 
@@ -41,7 +41,7 @@ Instructions: Write a function that returns true if a number is odd
               Use functions "isEven" and "not" somehow in the definition.
 ===================== */
 
-var isOdd = function() {};
+var isOdd = function(num) {return num % 2 != 0};
 
 console.log('isOdd success:', isOdd(4) === false);
 
